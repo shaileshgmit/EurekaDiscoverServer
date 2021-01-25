14df81305d79564bb961e93406825f16a821d3ca
Shailesh Gupta <shailesh.gupta@zensar.com>
2021-01-25 13:14:07 +0530

    adding files

---
+++ src/test/java/com/eureak/server/eureaknamingserver/EureakNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureak.server.eureaknamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EureakNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
